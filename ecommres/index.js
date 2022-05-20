388e643f41836d01fb0458cf99dd08a3a769c1f9
lima-akter754 <95654801+lima-akter754@users.noreply.github.com>
2022-05-21 00:32:57 +0600

    Add files via upload

---
+++ ecommres/index.js
@@ -0,0 +1,17 @@
+
+// *******  details section start *********\\
+
+
+let accordions = document.querySelectorAll('.accordion-container .accordion');
+
+accordions.forEach(acco =>{
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => { subAcco.classList.remove('active') });
+        acco.classList.add('active');
+        
+    }
+});
+
+
+// *******  details section end *********\\
+
